451e6bcc23e2a6354360ef7a9758ad720e563580
Das3013 <46927879+das6013@users.noreply.github.com>
2021-02-04 14:00:31 +0700

    Add files via upload

---
+++ Work file/ConsoleApplication4.cpp
@@ -0,0 +1,26 @@
+﻿#include <fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int n, g = 0, x = 1;
+    setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
+    char buff[50]; // буфер промежуточного хранения считываемого из файла текста
+    ifstream fin("tlist.txt"); // открыли файл для чтения
+
+    while (!fin.eof()) {
+        fin.getline(buff, 50);
+        n = strlen(buff);
+        for (int i = 0; i < n / 2; i++)
+            if (buff[i] == buff[n - 1 - i])  g++;
+        if (g == n / 2) cout << x << " stroka" << " palindrom " << buff << endl;
+        g = 0;
+        x++;
+
+
+
+    }
+    fin.close(); // закрываем файл
+}
